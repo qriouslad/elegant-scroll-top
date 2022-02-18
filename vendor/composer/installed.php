6d19d36683702aa5458aa981d9c97b1950684697
Bowo <wibowosulistio@gmail.com>
2022-02-18 13:45:11 +0700

    Add wp-settings library via composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'jeffreyvanrossum/wp-settings' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jeffreyvanrossum/wp-settings',
+            'aliases' => array(),
+            'reference' => '7f1d6ea46b5455f18b000f31dbb75ca3e3cfaab7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
